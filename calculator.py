f01bac991de6a13fcac7d7f44fa5a54ba10e51ca
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2025-11-11 14:33:14 +0800

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,6 +1,28 @@
 number1 = float(input("Enter first number: "))
 number2 = float(input("Enter second number: "))
+
+
+user=input("Enter the 1st Number")
+user1=input("Enter the 2nd Number")
+
+print("Select an operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+user2=input("Choose what operation you want to do")
+if user2 == "1":
+    #functions here
+    
 def division(a,b):
     return a/b
 
 print(division(number1,number2))
+
+def subtract_numbers(a, b):
+
+    return a - b
+
+result = subtract_numbers(10, 4)
+print("Result:", result)  

0874554949311f9c18b2f2bc6c32133bf347f686
BounzieBanana <luke.jamis@gmail.com>
2025-11-11 14:39:56 +0800

    calculator.py
    FIXED UPDATE with Addition

--- calculator.py
+++ calculator.py
@@ -1,9 +1,5 @@
-number1 = float(input("Enter first number: "))
-number2 = float(input("Enter second number: "))
-
-
-user=input("Enter the 1st Number")
-user1=input("Enter the 2nd Number")
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
 
 print("Select an operation:")
 print("1. Addition (+)")
@@ -11,18 +7,17 @@ print("2. Subtraction (-)")
 print("3. Multiplication (*)")
 print("4. Division (/)")
 
-user2=input("Choose what operation you want to do")
-if user2 == "1":
-    #functions here
-    
+operation = input("Choose what operation you want to do\n")
 def division(a,b):
     return a/b
+    
+def addition(a,b):
+    return a + b
 
-print(division(number1,number2))
-
-def subtract_numbers(a, b):
-
+def subtraction(a,b):
     return a - b
 
-result = subtract_numbers(10, 4)
-print("Result:", result)  
+
+if operation == "1":
+    result = addition(a,b)
+    print(result)

9fe02ebb557b36b72e2eb28eca4db580dc51a795
esclamadofrancereal-sys <esclamadofrancereal@gmail.com>
2025-11-11 14:18:00 +0800

    Update scanner.py

---
+++ scanner.py
@@ -0,0 +1,12 @@
+user=input("Enter the 1st Number")
+user1=input("Enter the 2nd Number")
+
+print("Select an operation:")
+print("1. Addition (+)")
+print("2. Subtraction (-)")
+print("3. Multiplication (*)")
+print("4. Division (/)")
+
+user2=input("Choose what operation you want to do")
+if user2 == "1":
+    #functions here
